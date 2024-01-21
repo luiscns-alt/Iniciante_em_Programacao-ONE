3b4764cafa28ccce1f0a0ab73fe10bf4e88badbc
Luis Carlos <luiscarlosnors@gmail.com>
2024-01-20 23:12:26 -0300

    Reiniciando o Jogo

--- Lógica de programação - explore funções e listas/app.js
+++ Lógica de programação - explore funções e listas/app.js
@@ -18,6 +18,20 @@ function limparCampo() {
     chute.value = '';
 }
 
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial()
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
 
 function verificarChute() {
     let chute = document.querySelector('input').value;

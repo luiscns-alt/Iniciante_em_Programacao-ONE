1043ee994b0ed1b792ae680ded466039b38328f5
Luis Carlos <luiscarlosnors@gmail.com>
2024-01-20 23:40:35 -0300

    Desafio 4

---
+++ Lógica de programação - explore funções e listas/Desafio_4/app.js
@@ -0,0 +1,9 @@
+let listaGenerica = [];
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+let nomes1 = ['Nome1', 'Nome2', 'Nome3'];
+console.log(nomes1[0]);
+let nomes2 = ['NomeA', 'NomeB', 'NomeC'];
+console.log(nomes2[1]);
+let nomes3 = ['NomeX', 'NomeY', 'NomeZ'];
+console.log(nomes3[nomes3.length - 1]);

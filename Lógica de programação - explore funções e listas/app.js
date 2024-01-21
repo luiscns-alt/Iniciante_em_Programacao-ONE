257789a444038a0ef6157d6026511dd0ccd317c5
Luis Carlos <luiscarlosnors@gmail.com>
2024-01-20 23:06:55 -0300

    Botão reiniciar

--- Lógica de programação - explore funções e listas/app.js
+++ Lógica de programação - explore funções e listas/app.js
@@ -13,6 +13,12 @@ function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
 
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+
 function verificarChute() {
     let chute = document.querySelector('input').value;
 
@@ -21,6 +27,7 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
@@ -28,5 +35,6 @@ function verificarChute() {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
         tentativas++;
+        limparCampo();
     }
 }
